d6a15e9f13c8098ef57dfee9a44bbfd509ccdedf
Byron45 <brfloresa@uce.edu.ec>
2026-01-23 19:47:00 -0500

    Taller 31

--- src/main/java/uce/edu/web/api/matricula/application/EstudianteService.java
+++ src/main/java/uce/edu/web/api/matricula/application/EstudianteService.java
@@ -1,10 +1,12 @@
 package uce.edu.web.api.matricula.application;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import uce.edu.web.api.matricula.application.representation.EstudianteRepresentation;
 import uce.edu.web.api.matricula.domain.Estudiante;
 import uce.edu.web.api.matricula.infraestructure.EstudianteRepository;
 
@@ -14,13 +16,16 @@ public class EstudianteService {
     @Inject
     private EstudianteRepository estudianteRepository;
 
-    public List<Estudiante> listarTodos() {
-        return this.estudianteRepository.listAll();
-
+    public List<EstudianteRepresentation> listarTodos() {
+        List<EstudianteRepresentation> list = new ArrayList<>();
+        for (Estudiante estu : this.estudianteRepository.listAll()) {
+            list.add(this.mapperTOER(estu));
+        }
+        return list;
     }
 
-    public Estudiante consultarPorId(Integer id) {
-        return this.estudianteRepository.findById(id.longValue());
+    public EstudianteRepresentation consultarPorId(Integer id) {
+        return this.mapperTOER(this.estudianteRepository.findById(id).orElse(null));
     }
 
     @Transactional
@@ -29,8 +34,12 @@ public class EstudianteService {
     }
 
     @Transactional //cuando genera cambios en la base de datos
-    public void actualizar(Integer id, Estudiante est) {
-        Estudiante estu = this.consultarPorId(id);
+    public void actualizar(Integer id, EstudianteRepresentation est) {
+        EstudianteRepresentation estuRep = this.consultarPorId(id);
+        if (estuRep == null) {
+            return;
+        }
+        Estudiante estu = this.mapperToEstudiante(estuRep);
         estu.setApellido(est.getApellido());
         estu.setNombre(est.getNombre());
         estu.setFechaNacimiento(est.getFechaNacimiento());
@@ -42,7 +51,11 @@ public class EstudianteService {
 
     @Transactional
     public void actualizarParcial(Integer id, Estudiante est) {
-        Estudiante estu = this.consultarPorId(id);
+        EstudianteRepresentation estuRep = this.consultarPorId(id);
+        if (estuRep == null) {
+            return;
+        }
+        Estudiante estu = this.mapperToEstudiante(estuRep);
         if (est.getApellido() != null) {
             estu.setApellido(est.getApellido());
         }
@@ -64,13 +77,35 @@ public class EstudianteService {
 
     @Transactional
     public void eliminar(Integer id) {
-        this.estudianteRepository.deleteById(id.longValue());
+        this.estudianteRepository.deleteById(id);
     }
 
     public List<Estudiante> buscarPorProvincia(String provincia, String genero) {
         //return this.estudianteRepository.find("provincia", provincia).list();
         return this.estudianteRepository.find("provincia = ?1 and genero = ?2", provincia, genero).list();
 
+    }
+
+    private EstudianteRepresentation mapperTOER(Estudiante estu) {
+        EstudianteRepresentation estuRep = new EstudianteRepresentation();
+        estuRep.setId(estu.getId());
+        estuRep.setApellido(estu.getApellido());
+        estuRep.setNombre(estu.getNombre());
+        estuRep.setFechaNacimiento(estu.getFechaNacimiento());
+        estuRep.setGenero(estu.getGenero());
+        estuRep.setProvincia(estu.getProvincia());
+        return estuRep;
+    }
+
+    private Estudiante mapperToEstudiante(EstudianteRepresentation estuRep) {
+        Estudiante estu = new Estudiante();
+        estu.setId(estuRep.getId());
+        estu.setApellido(estuRep.getApellido());
+        estu.setNombre(estuRep.getNombre());
+        estu.setFechaNacimiento(estuRep.getFechaNacimiento());
+        estu.setGenero(estuRep.getGenero());
+        estu.setProvincia(estuRep.getProvincia());
+        return estu;
     }
 
 }
--- src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
@@ -3,6 +3,7 @@ package uce.edu.web.api.matricula.interfaces;
 import java.util.List;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
@@ -15,13 +16,17 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.matricula.application.EstudianteService;
+import uce.edu.web.api.matricula.application.HijoService;
 import uce.edu.web.api.matricula.domain.Estudiante;
+import uce.edu.web.api.matricula.domain.Hijo;
 
 @Path("/estudiantes")
 public class EstudianteResource {
 
     @Inject
     private EstudianteService estudianteService;
+    @Inject
+    private HijoService hijoService;
 
     @GET
     @Path("")
@@ -41,6 +46,8 @@ public class EstudianteResource {
 
     @POST
     @Path("")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response guardar(Estudiante estu) {
         this.estudianteService.crear(estu);
         return Response.status(Response.Status.CREATED).entity(estu).build();
@@ -48,14 +55,17 @@ public class EstudianteResource {
 
     @PUT
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public Response actualizar(@PathParam("id") Integer id, Estudiante estu) {
         this.estudianteService.actualizar(id, estu);
-        return Response.status(209).entity(estu).build();
+        return Response.status(209).entity(null).build();
 
     }
 
     @PATCH
     @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
     public void actualizarParcial(@PathParam("id") Integer id, Estudiante estu) {
         this.estudianteService.actualizarParcial(id, estu);
     }
@@ -68,9 +78,16 @@ public class EstudianteResource {
 
     @GET
     @Path("/provincia/genero")
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Estudiante> buscarPorProvincia(@QueryParam("provincia") String provincia, @QueryParam("genero") String genero) {
         System.out.println("Listar por provincia y generoXXXXXXXX");
         return this.estudianteService.buscarPorProvincia(provincia, genero);
+    }
+
+    @GET
+    @Path("/{id}/hijos")
+    public List<Hijo> buscarPorIdEstudiante(@PathParam("id") Integer id) {
+        return this.hijoService.buscarPorIdEstudiante(id);
     }
 
 }
